bb147aa3b775f5d503cd4f9aca52e0e17c6783d9
Álex González <agonzalezro@gmail.com>
2014-11-04 17:39:38 +0000

    tmate conf

--- install.sh
+++ install.sh
@@ -79,6 +79,9 @@ warning_if_exists git
 ln -s $PWD/tmux.conf ~/.tmux.conf &>> /dev/null
 warning_if_exists tmux
 
+ln -s $PWD/tmate.conf ~/.tmate.conf &>> /dev/null
+warning_if_exists tmate
+
 ln -s $PWD/pdbrc ~/.pdbrc &>> /dev/null
 # Don't need to check
 
