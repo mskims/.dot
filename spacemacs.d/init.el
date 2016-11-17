baaa15ca8f25deb1c58863df0b44e93da01bbd22
Álex González <agonzalezro@gmail.com>
2016-11-17 13:20:32 +0100

    New default theme + font for Spacemacs

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -144,16 +144,17 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
+   dotspacemacs-themes '(molokai
+                         spacemacs-dark
                          phoenix-dark-mono
                          spacemacs-light)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Pro"
-                               :size 13
-                               :weight normal
+   dotspacemacs-default-font '("Inconsolata for Powerline"
+                               :size 14
+                               :weight light
                                :width normal
                                :powerline-scale 1.1)
    ;; The leader key
